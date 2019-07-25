39134daa7325df4d49e66f63b37f54f073914cc8
Andrew Drake <adrake@adrake.org>
2019-07-24 17:51:29 -0700

    Update dependencies.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = express();
 // app config
 app.set('trust proxy', true);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 
